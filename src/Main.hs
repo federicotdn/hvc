2adcc27d4665a9e0cdf799215d3fd12abbab62c8
Federico T <ftedinur@itba.edu.ar>
2015-12-01 17:51:03 -0300

    testing args parse

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
+import System.Environment
+import System.Exit
+import System.IO
+import qualified Crypto.Hash.SHA1 as SHA1
+
 main :: IO ()
-main = do
-  a <- getLine
-  putStrLn a
+main = getArgs >>= parse
+
+parse :: [String] -> IO ()
+parse ["init", dir] = putStrLn "dir"
+parse ["commit", dir] = putStrLn "commit"
+parse ["hash", dir] = usage
+parse xs = usage
+
+
+usage :: IO ()
+usage = putStrLn "usage"
